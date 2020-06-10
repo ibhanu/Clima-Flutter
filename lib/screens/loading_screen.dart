258dbe8ab7b1d5e618482492a56d0914dd1e858d
Bhanu Pratap <pratap.bhanu3434@gmail.com>
2020-06-11 03:43:03 +0530

    Added API

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:clima/services/location.dart';
+
+const apiKey = '08340e8075a856ae67fe5fb14b30be8f';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,17 +12,27 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude, longitide;
+  void getLocationData() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitide = location.longitude;
+
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitide&appid=$apiKey');
+
+    var networkData = await networkHelper.getData();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            //Get the current location
-          },
-          child: Text('Get Location'),
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -74,3 +74,7 @@ class _LocationScreenState extends State<LocationScreen> {
     );
   }
 }
+
+// double temp = decodedData['main']['temp'];
+// int id = jsonDecode(data)['weather'][0]['id'];
+// String name = jsonDecode(data)['name'];
